d894208b7073f51cdab30c92baebf040efaedd4d
Michał Flajszer <flajszermichal@hotmail.com>
2020-08-22 19:14:51 +0200

    first react exercise

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>My first React app</h1>
+      <div id="app">
+        <main class="app-content">
+          <h1>My first React app</h1>
+          <h2>Hello world!</h2>
+        </main>
       </div>
     )
   }
